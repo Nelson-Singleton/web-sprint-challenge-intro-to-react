3a6fe44c62d9e8da521cb532ca255cea6affab27
nelson-singleton <nsingleton@ymail.com>
2020-08-14 12:49:21 -0500

    Added styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect} from "react";
 import Charactercard from './Charactercard'
+import styled from 'styled-components';
 
 const Character = (props) => {
    
     const { starWarsData, setData } = props;
 
-    return(
+    const StyledSection = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    `;
+    
+    
 
+    return(
+        
         <div>
+            <h1>Heroes/Enemies of the Rebellion</h1>
+            <StyledSection>
+
+
+        
             {
                  starWarsData.map(ch => {
                     
@@ -16,9 +30,12 @@ const Character = (props) => {
                 })
                      
                                 
-               
+                    
             }
+            </StyledSection>
         </div>
+
+        
 
     );
 
--- starwars/src/components/Charactercard.js
+++ starwars/src/components/Charactercard.js
@@ -1,28 +1,52 @@
 import React, { useState, useEffect} from "react";
 import App from '../App'
 import Character from './Character'
+import styled from 'styled-components';
 
 
 const Charactercard = (props) => {
    
     const { card, setData } = props;
 
-    return (
+    
 
-        <div>
-            <h2>
-                {card.name}
-            </h2>
-            <h2>
-                {card.gender}
-            </h2>
-            <h3>
-                {card.height}
-            </h3>
-            <h3>
-                {card.mass}
-            </h3>
-        </div>
+    const StyledDiv = styled.div`
+    margin-top: 5%;
+    width: 45%;
+    text-align: center;
+    border: 5px solid white;
+    background-color: black;
+    color: white;
+    `;
+
+    const StyledH2 = styled.h2`
+    background-color: white;
+    color: black;
+    `;
+
+    const StyledH3 = styled.h3`
+    background-color: blue;
+    
+    `;
+
+    return (
+        
+        <StyledDiv>
+        
+            <StyledH2>
+                {`Name: ${card.name}`}
+            </StyledH2>
+            <StyledH3>
+                {`Gender: ${card.gender}`}
+            </StyledH3>
+            <StyledH3>
+                {`Height: ${card.height}`}
+            </StyledH3>
+            <StyledH3>
+                {`Weight: ${card.mass}`}
+            </StyledH3>
+        </StyledDiv>
+        
 
     );
 
