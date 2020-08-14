76752dc8d48982f92ffb5e559512f5ea21d57b5e
nelson-singleton <nsingleton@ymail.com>
2020-08-14 10:37:56 -0500

    Get request is working. State is set.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect} from "react";
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +11,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [starWarsData, setData] = useState([])
+
+useEffect(() => {
+  axios.get(`https://swapi.dev/api/people/`)  
+  .then(response => {
+    setData(response.data.results)
+    console.log(response.data.results)
+    debugger
+  })
+  .catch(err => {
+    debugger
+  })
+  .finally(() => {
+  
+  })
+  
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      
     </div>
   );
 }
 
 export default App;
+
+
